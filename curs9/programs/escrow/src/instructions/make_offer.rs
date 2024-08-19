3deb490e12c61dfb137bbf5d4cdcb74a1da24226
Denis-bit <72880982+Denis-bit@users.noreply.github.com>
2024-08-19 18:27:31 +0300

    Update make_offer.rs

--- curs9/programs/escrow/src/instructions/make_offer.rs
+++ curs9/programs/escrow/src/instructions/make_offer.rs
@@ -1,72 +1,62 @@
+use crate::{Offer, ANCHOR_DISCRIMINATOR};
 use anchor_lang::prelude::*;
-
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface,
-TransferChecked},
+    token_interface::{transfer_checked, Mint, TokenAccount, TokenInterface, TransferChecked},
 };
-
-use crate::{Offer, ANCHOR_DISCRIMINATOR};
 
 #[derive(Accounts)]
 #[instruction(id: u64)]
 pub struct MakeOffer<'info> {
     #[account(mut)]
     pub maker: Signer<'info>,
-
     #[account(mint::token_program = token_program)]
-    pub token_mint_a: InterfaceAccount<'info, Mint>, 
-
+    pub token_mint_a: InterfaceAccount<'info, Mint>,
     #[account(mint::token_program = token_program)]
     pub token_mint_b: InterfaceAccount<'info, Mint>,
-
     #[account(
-        mut,
-        associated_token::mint = token_min_a, 
-        asscoiated_token::authority = maker,
-        associated_token::token_program = token_program
-    )]
+mut,
+associated_token::mint = token_mint_a,
+associated_token::authority = maker,
+associated_token::token_program = token_program
+)]
     pub maker_token_account_a: InterfaceAccount<'info, TokenAccount>,
 
     #[account(
-        init, 
-        payer = maker,
-        space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE,
-        seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()], 
-        bump
-    )]
-    pub offer: Account<'info, Offer>, 
-
+init,
+payer = maker,
+space = ANCHOR_DISCRIMINATOR + Offer::INIT_SPACE,
+seeds = [b"offer", maker.key().as_ref(), id.to_le_bytes().as_ref()],
+bump
+)]
+    pub offer: Account<'info, Offer>,
     #[account(
-        init, 
-        payer = maker,
-        associated_token::mint = token_mint_a,
-        associated_token::authority = offer, 
-        associated_token::token_prgram = token_program
-    )]
-    pub vault: InterfaceAccount<'info, TokenAccount>, 
-
+init,
+payer = maker,
+associated_token::mint = token_mint_a,
+associated_token::authority = offer,
+associated_token::token_program = token_program
+)]
+    pub vault: InterfaceAccount<'info, TokenAccount>,
     pub associated_token_program: Program<'info, AssociatedToken>,
-    pub token_program: Interface<'info, TokenInterface>, 
+    pub token_program: Interface<'info, TokenInterface>,
     pub system_program: Program<'info, System>,
 }
 
 pub fn send_offered_tokens_to_vault(
     context: &Context<MakeOffer>,
-    token_a_offered_ammount: u64,
+    token_a_offered_amount: u64,
 ) -> Result<()> {
-    let transfer_accounts = TransferChecker {
+    let transfer_accounts = TransferChecked {
         from: context.accounts.maker_token_account_a.to_account_info(),
         mint: context.accounts.token_mint_a.to_account_info(),
         to: context.accounts.vault.to_account_info(),
         authority: context.accounts.maker.to_account_info(),
     };
-
     let cpi_context = CpiContext::new(
         context.accounts.token_program.to_account_info(),
         transfer_accounts,
     );
-
     transfer_checked(
         cpi_context,
         token_a_offered_amount,
@@ -74,10 +64,9 @@ pub fn send_offered_tokens_to_vault(
     )
 }
 
-pub fn save_offer(context: Context<MakeOffer>, id: u64, token_b_wanted_amount: u64) -> 
-Result<()> {
+pub fn save_offer(context: Context<MakeOffer>, id: u64, token_b_wanted_amount: u64) -> Result<()> {
     context.accounts.offer.set_inner(Offer {
-        id, 
+        id,
         maker: context.accounts.maker.key(),
         token_mint_a: context.accounts.token_mint_a.key(),
         token_mint_b: context.accounts.token_mint_b.key(),
@@ -86,4 +75,3 @@ Result<()> {
     });
     Ok(())
 }
-
